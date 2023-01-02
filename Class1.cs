c9e1340b05f33bb9626f34e25ac11c92631bd8a9
verhovensky <105872686+verhovenskyivan@users.noreply.github.com>
2023-01-02 23:47:32 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public static class Kata
+{
+    public static int CountSheeps(bool[] sheeps)
+    {
+        int count = 0;
+        foreach (bool sheep in sheeps)
+        {
+            if (sheep) count++;
+        }
+        return count;
+    }
+}
